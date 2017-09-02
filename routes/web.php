e8124789ea4e8e77752fddfcec50ac17b9ef7360
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-02 12:17:19 +0400

    added route wildcard to receive all react routes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
-Route::get('/', function () {
+ */
+Route::any('{catchall}', function () {
     return view('welcome');
-});
+})->where('catchall', '(.*)');
