1733770c4eec07e6753854c5a435579806c8d667
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-12-04 00:26:45 +0400

    added basic testing stuff

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -6,6 +6,14 @@ use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
 {
+    protected $baseUrl = 'http://localhost:8000';
+
+    protected $authUrl = 'api/v1/oauth/token';
+
+    protected $clientId;
+
+    protected $clientSecret;
+
     /**
      * Creates the application.
      *
@@ -16,6 +24,12 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+
+        $this->clientSecret   = env('UNIT_TEST_CLIENT_SECRET', '');
+        $this->clientId       = env('UNIT_TEST_CLIENT_ID', '');
+
+        $this->assertNotEmpty($this->clientId, "Client ID not provided. add in env file!");
+        $this->assertNotEmpty($this->clientSecret, "Client secret not provided. add in env file!");
 
         return $app;
     }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,104 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function authAsUser()
+    {
+        $userIdentifier = env('UNIT_TEST_USER', '');
+        $password = env('UNIT_TEST_USER_PASSWORD', '');
+        $response = $this->usernamePasswordSuccessfulAuth($userIdentifier, $password);
+        return $response;
+    }
+
+    protected function authAsPending()
+    {
+        $userIdentifier = env('UNIT_TEST_PENDING_USER', '');
+        $password = env('UNIT_TEST_USER_PENDING_PASSWORD', '');
+        $response = $this->usernamePasswordSuccessfulAuth($userIdentifier, $password);
+        return $response;
+    }
+
+    protected function authAsPassive()
+    {
+        $userIdentifier = env('UNIT_TEST_PENDING_USER', '');
+        $password = env('UNIT_TEST_USER_PENDING_PASSWORD', '');
+        $response = $this->usernamePasswordSuccessfulAuth($userIdentifier, $password);
+        return $response;
+    }
+
+    protected function userCredentialsBadAuth()
+    {
+        $userIdentifier = env('UNIT_TEST_USER', '');
+        $password = env('UNIT_TEST_USER_PASSWORD', '');
+
+        $response = $this->post($this->authUrl, [
+            'client_id' => 1,
+            'client_secret' => "idontknowmaybesomeinvalidclientsecret",
+            'grant_type' => 'password',
+            'username' => $userIdentifier,
+            'password' => $password,
+        ]);
+
+        return $response;
+    }
+
+    /**
+     * Make valid request for OAuth2 with password grant
+     */
+    protected function usernamePasswordSuccessfulAuth(string $userIdentifier, string $password)
+    {
+        $response = $this->post($this->authUrl, [
+            'client_id' => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'grant_type' => 'password',
+            'username' => $userIdentifier,
+            'password' => $password,
+        ]);
+
+        return $response;
+    }
+
+    public function callAsUser($method, $url, $data = [], $headers = [])
+    {
+        $authResponse = $this->authAsUser();
+        $accessTokenHeader = $this->parseAccessTokenFromResponseAndTranformToHeader($authResponse);
+        $headers = array_merge($headers, $accessTokenHeader);
+        $response = $this->json('GET',route('api::auth::user'),[],$headers);
+        return $this->json($method, $url, $data, $headers);
+    }
+
+    public function logoutUser($headers = [])
+    {
+        $headers = array_merge($headers, $this->accessTokenHeaderFromLocalVariable());
+        $response = $this->json('PATCH', route('api::auth::logout'), [], $headers);
+        return $response;
+    }
+
+    protected function parseAccessTokenFromResponse(TestResponse $response)
+    {
+        if (is_null($response)) {
+            return null;
+        }
+
+        $content = json_decode($response->getContent());
+
+        if (!isset($content->access_token)) {
+            return null;
+        }
+
+        return $content->access_token;
+    }
+
+    public function parseAccessTokenFromResponseAndTranformToHeader(TestResponse $response)
+    {
+        $this->accessToken = $this->parseAccessTokenFromResponse($response);
+
+        if (is_null($this->accessToken)) {
+            return [];
+        }
+
+        return [
+            'HTTP_Authorization' => 'Bearer ' . $this->accessToken,
+        ];
+    }
 }
