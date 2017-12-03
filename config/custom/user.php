5c91ea3179a477b5fb13bec06366bafcc821e729
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-12-04 00:27:42 +0400

    added user config with correct implementation in User model

---
+++ config/custom/user.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User info
+    |--------------------------------------------------------------------------
+    |
+    | All necessary information that's necessary for User
+    */
+
+    'status' => [
+        'active' => 'active',
+        'pending' => 'pending',
+        'passive' => 'passive',
+    ],
+];
