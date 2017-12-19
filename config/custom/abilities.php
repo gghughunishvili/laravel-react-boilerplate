2ab5b0409a468a6d1532103f6ec11ffbf940d0c3
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-12-20 00:20:15 +0400

    improved code style; added manage own and any user's ability

--- config/custom/abilities.php
+++ config/custom/abilities.php
@@ -21,7 +21,6 @@ $abilities = [];
 
 // Ability
 $abilityClass = Ability::class;
-
 $abilities[$abilityClass]['create'] = 1;
 $abilities[$abilityClass]['update'] = 2;
 $abilities[$abilityClass]['find'] = 3;
@@ -29,7 +28,6 @@ $abilities[$abilityClass]['give'] = 4;
 
 // Role
 $roleClass = Role::class;
-
 $abilities[$roleClass]['create'] = 5;
 $abilities[$roleClass]['update'] = 6;
 $abilities[$roleClass]['delete'] = 7;
@@ -38,8 +36,8 @@ $abilities[$roleClass]['give'] = 9;
 
 // User
 $userClass = User::class;
-
 $abilities[$userClass]['create'] = 10;
 $abilities[$userClass]['update'] = 11;
 $abilities[$userClass]['delete'] = 12;
 $abilities[$userClass]['find'] = 13;
+$abilities[$userClass]['manage-own'] = 14;
