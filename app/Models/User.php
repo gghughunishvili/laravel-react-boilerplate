9751f6d617ea4ecd229961213766601b80abe3e7
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-12-20 13:58:29 +0400

    added spatie permissions to user; added property descriptions for swagger

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,19 @@
 namespace App\Models;
 
 use App\Traits\UuidTrait;
+use Hash;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
-use Hash;
+use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * @SWG\Definition(definition="User", required={"name", "username", "email", "password"})
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable, UuidTrait;
+    use HasRoles;
 
     /**
      * Indicates if the IDs are auto-incrementing.
@@ -18,6 +23,36 @@ class User extends Authenticatable
      * @var bool
      */
     public $incrementing = false;
+
+    /**
+     * @SWG\Property(format="char:36")
+     * @var string
+     */
+    public $id;
+
+    /**
+     * @SWG\Property(example="John Doe")
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @SWG\Property(example="johndoo",minimum=4,maximum=55)
+     * @var string
+     */
+    public $username;
+
+    /**
+     * @SWG\Property(example="someone@example.com")
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @SWG\Property(minimum=6)
+     * @var string
+     */
+    public $password;
 
     /**
      * The attributes that are mass assignable.
