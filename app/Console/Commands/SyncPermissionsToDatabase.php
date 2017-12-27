1fc9ab5f3094be14e4fcc4b47bfc23fd53dd2295
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-12-28 01:05:35 +0400

    added command that will sync all permission to database from config file; command: php artisan sync:permissions

---
+++ app/Console/Commands/SyncPermissionsToDatabase.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Console\Command;
+
+class SyncPermissionsToDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:permissions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync all permissions from config to db and update admin roles to have all permissions';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     * if permissions haven't changed this command won't update permissions
+     * It drops all admin permissions' connection and then adds all of them back again
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info("Started permissions syncing");
+
+        $this->dropAllAdminPermissions();
+
+        $permissions = config('custom.permissions');
+
+        $syncedPermissions = collect([]);
+
+        foreach ($permissions as $wrapper => $subPermissions) {
+            foreach ($subPermissions as $permission => $id) {
+                $name = $permission . "-" . $wrapper;
+                $existedPermission = Permission::find($id);
+                if ($existedPermission && $name == $existedPermission->name) {
+                    // If exact permission exists in db
+                    $syncedPermissions->push($id);
+                    continue;
+                }
+
+                if ($existedPermission) {
+                    $existedPermission->delete();
+                }
+
+                // Also should be deleted if exists by this id
+                $existedIdPermission = Permission::find($id);
+                if ($existedIdPermission) {
+                    $existedIdPermission->delete();
+                }
+
+                $newPermission = $this->createAndSaveNewPermission($id, $name);
+                $syncedPermissions->push($id);
+            }
+        }
+
+        $this->deleteMismatchedPermissions($syncedPermissions);
+
+        $this->attachAllPermissionsToAdmin();
+        $this->info("Permissions sync has finished succesfully!");
+    }
+
+    protected function dropAllAdminPermissions()
+    {
+        $adminRole = Role::getByName('admin');
+        if ($adminRole) {
+            $adminRole->syncPermissions([]);
+        }
+    }
+
+    protected function attachAllPermissionsToAdmin()
+    {
+        $adminRole = Role::getByName('admin');
+        if ($adminRole) {
+            $permissions = Permission::all();
+            $adminRole->givePermissionTo($permissions);
+        }
+    }
+
+    protected function createAndSaveNewPermission($id, $name)
+    {
+        $newPermission = new Permission();
+        $newPermission->name = $name;
+        $newPermission->id = $id;
+        $newPermission->save();
+        return $newPermission;
+    }
+
+    protected function deleteMismatchedPermissions($syncedPermissions)
+    {
+        Permission::whereNotIn('id', $syncedPermissions)->delete();
+    }
+}
