28230353e5eaa4262c63845628b5ad54f5c421bd
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-01-01 21:23:19 +0400

    added couple more permissions for user, role and permission resources

--- config/custom/permissions.php
+++ config/custom/permissions.php
@@ -14,18 +14,29 @@
 
 $permissions = [];
 
+// Role
 $permissions['role']['create'] = 1;
 $permissions['role']['get'] = 2;
 $permissions['role']['find'] = 3;
 $permissions['role']['delete'] = 4;
-$permissions['role']['attach-permission-to'] = 5;
+$permissions['role']['find-permissions-for'] = 5;
+$permissions['role']['attach-permission-to'] = 6;
+$permissions['role']['detach-permission-from'] = 7;
 
-$permissions['user']['create'] = 6;
-$permissions['user']['update'] = 7;
-$permissions['user']['get'] = 8;
-$permissions['user']['find'] = 9;
-$permissions['user']['delete'] = 10;
-$permissions['user']['attach-permisssion-to'] = 11;
-$permissions['user']['attach-role-to'] = 12;
+// Permission
+$permissions['permission']['find'] = 8;
+
+// User
+$permissions['user']['create'] = 9;
+$permissions['user']['update'] = 10;
+$permissions['user']['get'] = 11;
+$permissions['user']['find'] = 12;
+$permissions['user']['delete'] = 13;
+$permissions['user']['find-permissions-for'] = 14;
+$permissions['user']['attach-permisssion-to'] = 15;
+$permissions['user']['detach-permisssion-from'] = 16;
+$permissions['user']['attach-role-to'] = 17;
+$permissions['user']['detach-role-from'] = 18;
+
 
 return $permissions;
